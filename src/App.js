938ad60bc91c7cfecfdaa1b215e0979fffd17882
K4RU3 <pzKARUzq2@gmail.com>
2024-10-20 14:55:38 +0900

    remove logo files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+import Home from './components/Home';
 
 function App() {
+  const [DisplayComponent, setDisplayComponent] = useState(()=>Home)
+
+  const changeComponent = (component) => {
+    if(typeof component === "function"){
+      setDisplayComponent(()=>component)
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {DisplayComponent && <DisplayComponent changeComponent={changeComponent}/>}
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Home({changeComponent}) {
+  return (
+    <div>
+        <h1 style={{
+            position:"absolute",
+            top:"10%",
+            left:"50%",
+            transform:"translate(-50%, -50%)",
+            fontSize:"6rem",
+            fontWeight:"bold",
+            textAlign:"center"
+        }}>SpellWars</h1>
+
+    </div>
+  )
+}

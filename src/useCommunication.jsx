c8acd3508294d0799280998619122cda6499a32f
K4RU3 <pzKARUzq2@gmail.com>
2024-12-03 16:10:53 +0900

    usecommunication update

--- src/useCommunication.jsx
+++ src/useCommunication.jsx
@@ -1,6 +1,4 @@
-import { useState, useRef, useCallback } from 'react';
 import { useAppContext } from './AppContext';
-import useWebSocket from './useWebScoket';
 
 const useCommunication = () => {
     
